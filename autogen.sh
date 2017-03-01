fd10677fb767b18f67e1a3babf74f9d7180fda8b
Cosimo Cecchi <cosimo@endlessm.com>
2017-03-01 15:13:50 -0800

    build: fix copy/paste typo

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Bootstrap script for Endless OS showmehow-service
+# Bootstrap script for Endless OS coding-discovery-center
 # Run this script on a clean source checkout to get ready for building.
 
 test -n "$srcdir" || srcdir=`dirname "$0"`
